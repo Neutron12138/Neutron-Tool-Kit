67d6de687bf4ea12168f91a1d76e9c725950b1ca
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-01-05 20:23:07 +0800

    Add files via upload

---
+++ src/Base.hpp
@@ -0,0 +1,8 @@
+#ifndef __NEUTRON_BASE_HPP__
+#define __NEUTRON_BASE_HPP__
+
+#include "Base/Object.hpp"
+#include "Base/Type.hpp"
+#include "Base/Exception.hpp"
+
+#endif
---
+++ src/Version.hpp
@@ -0,0 +1,22 @@
+#ifndef __NEUTRON_VERSION_HPP__
+#define __NEUTRON_VERSION_HPP__
+
+// 版本号
+#define NEUTRON_VERSION "1.4"
+#define NEUTRON_VERSION_MAJOR "1"
+#define NEUTRON_VERSION_MINOR "4"
+
+// Alpha版本
+#define NEUTRON_VERSION_IS_ALPHA true
+#define NEUTRON_VERSION_ALPHA "1"
+// Beta版本
+#define NEUTRON_VERSION_IS_BETA false
+#define NEUTRON_VERSION_BETA "-"
+// Release版本
+#define NEUTRON_VERSION_IS_RELEASE false
+#define NEUTRON_VERSION_RELEASE "-"
+
+// 版本全称
+#define NEUTRON_VERSION_FULL "1.4 Alpha 1"
+
+#endif
