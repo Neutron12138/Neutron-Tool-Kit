8249349894abebe76fdd3b48c921c37261b70e8e
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-01-07 11:00:32 +0800

    Delete src directory

--- src/Base.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef __NEUTRON_BASE_CPP__
-#define __NEUTRON_BASE_CPP__
-
-#include "Base.hpp"
-
-#include "Base/Object.cpp"
-#include "Base/Exception.cpp"
-
-#endif
--- src/Base.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __NEUTRON_BASE_HPP__
-#define __NEUTRON_BASE_HPP__
-
-#include "Base/Object.hpp"
-#include "Base/Type.hpp"
-#include "Base/Exception.hpp"
-
-#endif
